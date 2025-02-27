6adfd1bf857459ee93dccab1bd3a9e45c22b73cc
dlrjsgh367 <dlrjsgh367@gmail.com>
2025-02-27 18:51:00 +0900

    Fix: Invalid bean definition with name 'userMapper' - 버전 문제 였다 .. - 삽질 오지게 했네 !!!
    https://stackoverflow.com/questions/79085567/org-springframework-beans-factory-beandefinitionstoreexception-invalid-bean-def
    
    + 추가
    - 버전 문제가 원흉이 아니였음
    - 에러 톺아봐야함

--- src/main/java/com/geonho/moneyflow/MoneyflowApplication.java
+++ src/main/java/com/geonho/moneyflow/MoneyflowApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MoneyflowApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MoneyflowApplication.class, args);
     }
-
 }
--- src/main/java/com/geonho/moneyflow/repository/UserMapper.java
+++ src/main/java/com/geonho/moneyflow/repository/UserMapper.java
@@ -1,11 +1,15 @@
 package com.geonho.moneyflow.repository;
 
 import com.geonho.moneyflow.dto.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface UserMapper {
+
+    @Insert("INSERT INTO user (username, email, password) VALUES (#{username}, #{email}, #{password})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
     void save(User user);
+
+    @Select("SELECT * FROM user WHERE email = #{email}")
     User findByEmail(@Param("email") String email);
 }
