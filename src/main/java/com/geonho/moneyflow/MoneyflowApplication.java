c15a21f20877fdfd69428cc56fd077ed6a09ddf0
dlrjsgh367 <dlrjsgh367@gmail.com>
2025-02-28 10:12:33 +0900

    Fix: xml 매핑 오류

--- src/main/java/com/geonho/moneyflow/MoneyflowApplication.java
+++ src/main/java/com/geonho/moneyflow/MoneyflowApplication.java
@@ -1,10 +1,12 @@
 package com.geonho.moneyflow;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@MapperScan("com.geonho.moneyflow.repository")
 public class MoneyflowApplication {
     public static void main(String[] args) {
         SpringApplication.run(MoneyflowApplication.class, args);
--- src/main/java/com/geonho/moneyflow/repository/UserMapper.java
+++ src/main/java/com/geonho/moneyflow/repository/UserMapper.java
@@ -1,15 +1,9 @@
 package com.geonho.moneyflow.repository;
 
 import com.geonho.moneyflow.dto.User;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Param;
 
-@Mapper
 public interface UserMapper {
-
-    @Insert("INSERT INTO user (username, email, password) VALUES (#{username}, #{email}, #{password})")
-    @Options(useGeneratedKeys = true, keyProperty = "id")
     void save(User user);
-
-    @Select("SELECT * FROM user WHERE email = #{email}")
     User findByEmail(@Param("email") String email);
 }
